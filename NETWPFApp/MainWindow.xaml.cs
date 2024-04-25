0864cbd121877fd6f54bbb51c48a1a4d935772e2
Erik Movsesyan <erik.movsesyan@ihsmarkit.com>
2024-04-26 00:25:45 +0400

    Task1 WPF app done

---
+++ NETWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace NETWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void GoButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameInput.Text;
+            var isUsernameValid = ValidateUsername(username);
+            var isInputWhiteSpace = string.IsNullOrWhiteSpace(username);
+
+            MessageBox.Show($"{(isInputWhiteSpace ? "No username provided!"
+                : isUsernameValid
+                    ? $"Hello, {username}"
+                    : "Invalid Username")}");
+        }
+
+        static bool ValidateUsername(string username)
+        {
+            return Regex.IsMatch(username, @"^([a-zA-Z0-9.\-_]){5,20}$");
+        }
+    }
+}
